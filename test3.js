02512a14bc0b32b4e77888b2a9be794d33f5c709
Sirwin <sirwin2win@gmail.com>
2026-03-02 12:14:15 +0100

    test

---
+++ test3.js
@@ -0,0 +1,7 @@
+/*
+1. Why do we increment or decrement when we are running a loop
+2. What is the difference between a parameter and an argument
+3. What do you understand by the term, method
+4. What are the ways of creating objects
+5. Given the string, const num = '23456'; arrange thus 65432
+*/
